929c53f42973133334ac482072b1284821761d1c
MaxSmakov <maxsmakov@gmail.com>
2017-07-12 12:15:20 +0300

    new5

--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace  BrainGames\Cli;
+namespace BrainGames\Cli;
 
-//require_once __DIR__ . '/../php-cli-tools/lib/cli/cli.php';
-require_once __DIR__ . '/../vendor/autoload.php';
 use function \cli\line;
 
-function run(){
-  line('Welcome to the Brain Game!');
-  $name = \cli\prompt('May I have your name?');
-  line("Hello, %s!", $name);
+function run()
+{
+    line('Welcome to the Brain Games!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s", $name);
 }
