f7b101a463f2e46ba2925b6267ed726663cba085
MaxSmakov <maxsmakov@gmail.com>
2017-07-13 11:58:01 +0300

    brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -10,3 +10,43 @@ function run()
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s", $name);
 }
+
+
+const ANSWERS_COUNT = 3;
+
+function runEven()
+{
+  line('Welcome to the Brain Games!');
+  $name = \cli\prompt('May I have your name?');
+  line("Hello, %s", $name);
+    for ($i = 1; $i <= ANSWERS_COUNT; $i++) {
+        step($name);
+    }
+
+    line("Congratulations, %s", $name);
+}
+
+
+function checkAnswer($number, $answer)
+{
+    if (($number % 2 === 0 && $answer == "yes") || ($number % 2 != 0 && $answer == "no")) {
+        return true;
+  }
+    return false;
+}
+
+
+function step($name)
+{
+    $num = rand(1, 100);
+    line('Question: ' . $num);
+    $ans = \cli\prompt("Your answer is");
+    if (checkAnswer($num, $ans)) {
+        line('Correct!');
+        line();
+    } else {
+        line('"%s" - is wrong answer!', $ans);
+        line("Let's try again, %s!", $name);
+        exit();
+    }
+}
